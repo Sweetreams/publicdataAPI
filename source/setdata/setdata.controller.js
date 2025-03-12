27f52aa48f33bb300a25619c9b0179725ca24ee2
Sweetreams <pashasurov12345@gmail.com>
2025-03-12 17:00:06 +0700

    apply getDataSet

--- source/setdata/setdata.controller.js
+++ source/setdata/setdata.controller.js
@@ -30,29 +30,50 @@ router.get('/getdata', JWTvalidate, async (req, res) => {
 })
 
 router.get('/getdataset', JWTvalidate, async (req, res) => {
-    try {
-        const data = await setService.getDataSetID(req.query.id)
-        return res.status(200).json({
-            status: state.SUCCESS,
-            data: {
-                code: 200,
-                message: {
-                    data: data[0].data.data,
-                    title: data[0].data.title
-                }
-            }
-        })
-    } catch (error) {
-        return res.status(400).json({
-            status: state.ERROR,
-            error: {
-                code: 400,
-                error_message: error,
-                message: 'Произошла ошибка'
-            }
-        })
-    }
+    const data = await setService.getDataSet()
+    return res.status(200).json({
+        data: data
+    })
+    // try {
+        // const data = await setService.getDataSetID(req.query.id)
+        
+        // return res.status(200).json({
+        //     status: state.SUCCESS,
+        //     data: {
+        //         code: 200,
+        //         message: {
+        //             data: data[0].data.data,
+        //             title: data[0].data.title
+        //         }
+        //     }
+        // })
+    // } catch (error) {
+    //     console.log(error)
+    //     return res.status(400).json({
+    //         status: state.ERROR,
+    //         error: {
+    //             code: 400,
+    //             error_message: error,
+    //             message: 'Произошла ошибка'
+    //         }
+    //     })
+    // }
+})
 
+//TODO: Добавить проверку на admin
+
+router.post('/createset', JWTvalidate, async(req, res) => {
+    console.log(typeof req.body)
+    await setService.createSet(req.body)
+})
+
+router.post('/createsetdata', JWTvalidate, async(req, res) => {
+    const data = await setService.createDataSet(req.body)
+    console.log(data)
+})
+
+router.delete('/deletedataset', JWTvalidate, async(req, res) => {
+    await setService.deleteDataSetID(req.body?.id)
 })
 
 export const setRouter = router
--- source/setdata/setdata.service.js
+++ source/setdata/setdata.service.js
@@ -8,8 +8,37 @@ export class SetData {
         return this.prisma.setDB.findMany()
     }
 
-    getDataSetID(id){
+    getDataSet() {
+        return this.prisma.dataSet.findMany()
+    }
+
+    getDataSetID(id) {
         return this.prisma.dataSet.findMany({
+            where: {
+                id_set: Number(id)
+            }
+        })
+    }
+
+    createSet(dataset) {
+        return this.prisma.setDB.create({
+            data: {
+                data: dataset
+            }
+        })
+    }
+
+    createDataSet(dataset, id) {
+        return this.prisma.setDB.create({
+            data: {
+                id_set: id,
+                data: dataset
+            }
+        })
+    }
+
+    deleteDataSetID(id) {
+        return this.prisma.dataSet.deleteMany({
             where: {
                 id_set: Number(id)
             }
